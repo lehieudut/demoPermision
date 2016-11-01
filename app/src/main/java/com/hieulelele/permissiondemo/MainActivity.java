80306aa387eb2287e93a8fda83fc687afbfdca9d
HieuLe <lehieudut@gmail.com>
2016-11-01 22:40:42 +0700

    add camera

--- app/src/main/java/com/hieulelele/permissiondemo/MainActivity.java
+++ app/src/main/java/com/hieulelele/permissiondemo/MainActivity.java
@@ -1,13 +1,30 @@
 package com.hieulelele.permissiondemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int CAMERA_REQUEST = 100;
+    Button mButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButton = (Button) findViewById(R.id.mButton);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCamera();
+            }
+        });
+    }
+
+    private void showCamera() {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
     }
 }
