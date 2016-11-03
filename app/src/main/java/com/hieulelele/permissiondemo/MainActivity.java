00459b8e0323f43763a358c5b6eb86e983aa0893
lehieudut <lehieudut@gmail.com>
2016-11-03 19:21:27 +0700

    test

--- app/src/main/java/com/hieulelele/permissiondemo/MainActivity.java
+++ app/src/main/java/com/hieulelele/permissiondemo/MainActivity.java
@@ -1,13 +1,21 @@
 package com.hieulelele.permissiondemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int CAMERA_REQUEST = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void showCamera() {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
     }
 }
